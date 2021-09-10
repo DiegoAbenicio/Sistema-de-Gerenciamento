35298f5e4b17176f9d0429184ca427609d135427
DiegoAbenicio <diego.abenicio1@gmail.com>
2021-09-09 21:03:51 -0300

    Mapped maps

---
+++ src/main/java/one/digitalinovation/personapi/entity/Person.java
@@ -0,0 +1,40 @@
+package one.digitalinovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fisrtName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = false)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+
+
+
+
+}
---
+++ src/main/java/one/digitalinovation/personapi/entity/Phone.java
@@ -0,0 +1,31 @@
+package one.digitalinovation.personapi.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+
+}
